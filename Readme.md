165cb86e5dc574908bdddaa504620103d7274d43
py563 <maredukondaprajwal@gmail.com>
2019-01-16 14:48:15 -0500

    convert to python3 and naming files change

--- Readme.md
+++ Readme.md
@@ -1,2 +1,13 @@
-Executes script to download notes from study english courses offered by ABC Australia learn english series.
-uses python requests to download and saves a pdf file
+## Python based scripts to help gathering resources for IELTS exam
+
+[ABC English Learning Series](https://www.youtube.com/watch?v=p1UeGt-OtWE) 
+-- $ python3 downloadABCEngLearnSeriesNotes.py
+Executes [script] (downloadABCEngLearnSeriesNotes.py) to download notes from study english courses offered by Australia Broadcasting Corp(ABC) learn english series.
+uses python requests to download and saves a pdf file to notes folder in repository
+Urls are working as of 01/16/2019
+ 
+### Minimun Requirements to run the scripts and view notes
+- Python 3.5 or later
+- Adode Reader or any PDF reader
+
+Taking requests and passively trying to add other resources, meanwhile everyone is welcome to add.
--- downloadABCEngLearnSeriesNotes.py
+++ downloadABCEngLearnSeriesNotes.py
@@ -5,38 +5,42 @@ def loopFileLinks(file_url, file_name,series_no):
     if(series_no == 3):
         for x in range(3001,3027):
             file_url1 = file_url.format(x)
-            print file_url1
+            print(file_url1)
             r = requests.get(file_url1, stream = True)
             file_name1 = file_name.format(x)
             if r.ok and not os.path.exists(file_name1):
-                print file_name1
+                print(file_name1)
                 with open(file_name1,"wb") as pdf:
                     for chunk in r.iter_content(chunk_size=1024):
                         # writing one chunk at a time to pdf file
                         if chunk:
                             pdf.write(chunk)
-                print x
+                print(x)
     else:
         for x in range(1,27):
             for url1 in file_url:
                 file_url1 = url1.format(x)
-                print file_url1
+                print(file_url1)
                 r = requests.get(file_url1, stream = True)
                 file_name1 = file_name.format(x)
                 if r.ok and not os.path.exists(file_name1):
-                    print file_name1
+                    print(file_name1)
                     with open(file_name1,"wb") as pdf:
                         for chunk in r.iter_content(chunk_size=1024):
                             # writing one chunk at a time to pdf file
                             if chunk:
                                 pdf.write(chunk)
-                    print x
+                    print(x)
                     break
 
 if(__name__ == "__main__"):
     #begin
-    #series 1
-    series_file_name = 'IELTS_10{:02}_notes.pdf'
+    # creates a specific folder for the resource here ABC Learn English
+    dir_path = os.path.join('notes/ABCLearnEnglishSeries')
+    os.makedirs(dir_path,exist_ok=True)
+    # notes are named based on the episode or activity number in the videos
+    # series 1 
+    series_file_name = os.path.join(dir_path,'IELTS_10{:02}_notes.pdf')
     series_file_url = [
         'http://tv.australiaplus.com/learn-english/series-1/oct-16/study-notes/ep{}_studynotes_topdf.pdf',
         'http://tv.australiaplus.com/learn-english/series-1/oct-16/study-notes/ep{}_notes_v2.pdf',
@@ -44,7 +48,8 @@ if(__name__ == "__main__"):
         'http://tv.australiaplus.com/learn-english/series-1/oct-16/study-notes/ep{}(old17)_studynotes_topdf.pdf'
         ]
     loopFileLinks(series_file_url, series_file_name , 1)
-    series_activities_file_name = 'IELTS_10{:02}_activity.pdf'
+    # activity file names end with _activity similarly _notes for instructor notes
+    series_activities_file_name = os.path.join(dir_path,'IELTS_10{:02}_activity.pdf')
     series_activities_file_url = [
         'http://tv.australiaplus.com/learn-english/series-1/oct-16/activity/ep{}_activitysheet1_topdf.pdf',
         'http://tv.australiaplus.com/learn-english/series-1/oct-16/activity/ep{}_activitysheet_topdf.pdf',
@@ -53,7 +58,7 @@ if(__name__ == "__main__"):
         ]
     loopFileLinks(series_activities_file_url, series_activities_file_name,1)
     #series 2
-    series_file_name = 'IELTS_20{:02}_notes.pdf'
+    series_file_name = os.path.join(dir_path,'IELTS_20{:02}_notes.pdf')
     series_file_url = [
         'http://tv.australiaplus.com/learn-english/series-2/nov-16/study-notes/se2ep{}_studynotes_topdf.pdf',
         'http://tv.australiaplus.com/learn-english/series-1/oct-16/study-notes/ep{}_studynotes_topdf.pdf',
@@ -61,7 +66,7 @@ if(__name__ == "__main__"):
         'http://tv.australiaplus.com/learn-english/series-2/nov-16/study-notes/se2_episode%20{}%20study%20notes%20to%20pdf.pdf'
         ]
     loopFileLinks(series_file_url, series_file_name,2)
-    series_activities_file_name = 'IELTS_20{:02}_activity.pdf'
+    series_activities_file_name = os.path.join(dir_path,'IELTS_20{:02}_activity.pdf')
     series_activities_file_url = [
         'http://tv.australiaplus.com/learn-english/series-2/nov-16/activity/series2_ep{}_activitysheet_topdf.pdf',
         'http://tv.australiaplus.com/learn-english/series-1/oct-16/activity/ep{}_activitysheet_topdf.pdf',
@@ -69,10 +74,10 @@ if(__name__ == "__main__"):
         ]
     loopFileLinks(series_activities_file_url, series_activities_file_name,2)
     #series 3
-    series_file_name = 'IELTS_{}_notes.pdf'
+    series_file_name = os.path.join(dir_path,'IELTS_{}_notes.pdf')
     series_file_url = 'http://tv.australiaplus.com/learn-english/series-3/study-notes/s{}_notes.pdf'
     loopFileLinks(series_file_url, series_file_name,3)
-    series_activities_file_name = 'IELTS_{}_activity.pdf'
+    series_activities_file_name = os.path.join(dir_path,'IELTS_{}_activity.pdf')
     series_activities_file_url = 'http://tv.australiaplus.com/learn-english/series-3/activity/s{}_activities.pdf'
     loopFileLinks(series_activities_file_url, series_activities_file_name,3)
     #end
